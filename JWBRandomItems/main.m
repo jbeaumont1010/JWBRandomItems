8e7991de961f24376fa1aad5a9f6ea0638f755f0
Joseph Beaumont <josephbeaumont@Macintosh-3.local>
2015-01-31 14:11:49 -0500

    Initial Commit

---
+++ JWBRandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JWBRandomItems
+//
+//  Created by Joseph Beaumont on 1/31/15.
+//  Copyright (c) 2015 Joseph Beaumont. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
